d29dd4fe59319026e41399f5c368885182b3630c
Abdullah ULUCAK <ulucakabdullahulucak@gmail.com>
2025-09-24 12:13:37 +0300

    ana proje olusturuldu ilk taslak tasarımına baslandın

---
+++ android/app/src/main/kotlin/com/example/flutter_design_draft/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_design_draft
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Babble_App/MainPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mainpage extends StatefulWidget {
+  const Mainpage({super.key});
+
+  @override
+  State<Mainpage> createState() => _MainpageState();
+}
+
+class _MainpageState extends State<Mainpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
